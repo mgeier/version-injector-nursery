e1dda583bf1f2054ea75aa9ac6fa0904eea9768e
Matthias Geier <Matthias.Geier@gmail.com>
2024-10-05 19:05:19 +0200

    Add TOML file

--- src/version_injector.py
+++ src/version_injector.py
@@ -1,8 +1,32 @@
 import importlib.metadata
 from pathlib import Path
 
+import tomlkit
+
 __version__ = '0.0.0'
 
+# TODO: possible actions:
+#       - new build has been created
+#         - if already in TOML: inject versions only into the new build
+#         - if not yet in TOML: add version/variant, inject versions everywhere
+#       - manual trigger, inject versions everywhere
+
+# TODO: before each injection batch:
+#       - delete subdirectories that are not listed in TOML
+#       - delete temporary versions after given delay (based on git information?)
+#         "expire_in": "6 months and 4 days", "never"
+#         or after a given number is reached?
+
+# TODO: when parsing TOML:
+#       - check that all names are unique
+
+def read_toml():
+    f = Path('version-injector.toml')
+    # If file doesn't exist in current directory -> exception
+    config = tomlkit.load(f.open())
+    print(config['vanguard'])
+    print(config['versions'])
+    print(config['variants'])
 
 # TODO: get directory from command line arg or iterate all directories
 # TODO: check config file for available versions etc.
@@ -45,4 +69,5 @@ def inject():
 
 
 if __name__ == '__main__':
+    read_toml()
     inject()
