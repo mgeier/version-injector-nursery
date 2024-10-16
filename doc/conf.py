377c3540ccefa84d4b053d9529c73da1bde423d1
Matthias Geier <Matthias.Geier@gmail.com>
2024-10-16 20:56:31 +0200

    Build PDF

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,7 @@
+project = 'version-injector'
 html_theme = 'insipid'
 html_sidebars = {
     '**': ['globaltoc.html', 'separator.html', 'versions.html']
 }
 templates_path = ['_templates']
+latex_engine = 'xelatex'
